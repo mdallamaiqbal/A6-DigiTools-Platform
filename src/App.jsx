f9341f1234fb86b461a30276d1f0b07fdbfb52df
mdallamaiqbal <bd999plus13@gmail.com>
2026-03-30 21:21:14 +0600

    Ready structure and add taiwind css, daisyUI and font manrope

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+   <h2 className='text-2xl font-semibold '>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit voluptatum accusantium non dolorum, dolores corporis inventore esse distinctio est exercitationem!</h2>
+    </>
+  )
+}
+
+export default App
